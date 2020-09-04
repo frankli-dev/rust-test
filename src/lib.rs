fb0a95291912853ce0e90831d562f589556c68cd
Frank Li <frankli.consult@gmail.com>
2020-09-04 10:39:01 -0400

    Complete the task

--- src/lib.rs
+++ src/lib.rs
@@ -3,31 +3,63 @@ use seed::*;
 
 #[derive(Default)]
 struct Model {
-    text_to_show: String,
+    text: String,
+    items: Vec<String>
 }
 
 #[derive(Clone)]
 enum Msg {
     ChangeText(String),
+    AddItem,
+    ClearItems,
+    RemoveItem(usize)
 }
 
 fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
     use Msg::*;
 
     match msg {
-        ChangeText(new_text) => model.text_to_show = new_text,
+        ChangeText(new_text) => model.text = new_text,
+        AddItem => {
+            if model.text.len() > 0 {
+                model.items.push(model.text.clone());
+                model.text.clear();
+            }
+        },
+        ClearItems => model.items.clear(),
+        RemoveItem(index) => {
+            model.items.remove(index);
+        }
     }
 }
 
-fn view(model: &Model) -> Node<Msg> {
-    div![
-        input![
-            attrs! {
-                At::Placeholder => "Enter some text..."
-            },
-            input_ev(Ev::Input, Msg::ChangeText),
-        ],
-        div![&model.text_to_show]
+fn view(model: &Model) -> Vec<Node<Msg>> {
+    vec![
+        img![C!["logo"], attrs!{At::Src => "https://cameras.liveviewtech.com/img/LVLogo_small.png"}],
+        div![
+            C!["main"],
+            input![
+                attrs! {
+                    At::Placeholder => "Enter some text...",
+                    At::Value => &model.text
+                },                
+                input_ev(Ev::Input, Msg::ChangeText),
+            ],
+            div![
+                C!["buttons"],
+                button![C!["btn-save"], "Save", ev(Ev::Click, move |_| Msg::AddItem)],
+                button![C!["btn-clear"], "Clear", ev(Ev::Click, move |_| Msg::ClearItems)],
+            ],
+            ul![
+                C!["items-list"],
+                model.items.iter().enumerate().map(|(index, item)| {
+                    li![
+                        item,
+                        button![C!["btn-remove"], "X", ev(Ev::Click, move |_| Msg::RemoveItem(index))]
+                    ]
+                })
+            ]
+        ]
     ]
 }
 
